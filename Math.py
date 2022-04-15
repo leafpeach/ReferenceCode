b4afe01e691a68e4c7f4aac7ab95be118952bdc9
Leif! <42683701+leafpeach@users.noreply.github.com>
2022-04-15 17:19:16 -0400

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,32 @@
+#The min() and max() function
+
+x = min(5,10,25)
+y = max(5,10,25)
+
+print(x, y)
+
+
+#The absolute Value
+z = abs(-69)
+
+print(z)
+
+#Power
+power = pow(4, 6)
+print(power)
+
+#Built-in math module
+
+import math
+
+squareRoot = math.sqrt(64)
+
+print(squareRoot)
+
+ceiling = math.ceil(1.4) #returns 2
+floor = math.floor(1.4) #returns 1
+print(ceiling, floor)
+
+what_is_pi = math.pi
+
+print(what_is_pi)
